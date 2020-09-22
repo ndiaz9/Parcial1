9bfca63b63680bb7b58b05e58686f3c94557bbaa
ndiaz9 <na.diaz@uniandes.edu.co>
2020-09-22 09:11:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const url = "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
 const title = document.getElementById("title-main");
 
+fetch(url).then(response => response.json()).then(response => console.log(response)).catch(error => console.log(error));
+
 function fillBurgers(){
     title.innerHTML = "Burgers";
 }
